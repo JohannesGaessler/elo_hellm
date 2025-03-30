5a076942a42021d28210f9319ece77f37d1b1ecb
Johannes Gäßler <johannesg@5d6.de>
2025-03-30 14:31:09 +0200

    post-hoc min-p

--- analysis.py
+++ analysis.py
@@ -13,6 +13,16 @@ labels = np.array([ex["answer"] for ex in mmlu["test"]])
 
 models: List[str] = sorted(os.listdir(DIR_IN))
 
+
+def sample_min_p(predictions: np.ndarray, min_p: float) -> np.ndarray:
+    max_p = np.max(predictions, axis=1)
+    filter_array = predictions >= np.reshape(max_p * min_p, max_p.shape + (1,))
+    predictions = predictions * filter_array
+    sum_p = np.sum(predictions, axis=1)
+    predictions /= np.reshape(sum_p, max_p.shape + (1,))
+    return predictions
+
+
 for model in models:
     dir_in_m: str = os.path.join(DIR_IN, model)
     quantizations: List[str] = sorted(os.listdir(dir_in_m))
@@ -20,6 +30,8 @@ for model in models:
         predictions = np.load(os.path.join(dir_in_m, quant, "mmlu", "predictions.npy"))
         assert predictions.shape[0] == labels.shape[0]
         assert predictions.shape[1] == 4
+
+        # predictions = sample_min_p(predictions, 0.9)
 
         probs_label = predictions[np.arange(predictions.shape[0]), labels]
         print(f"====== {model}-{quant} ======")
