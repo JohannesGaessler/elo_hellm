25c58ffb1d71ca8471f464706d20dbce21475c2b
Johannes Gäßler <johannesg@5d6.de>
2025-04-11 23:43:56 +0200

    WIP

--- bench.py
+++ bench.py
@@ -18,7 +18,21 @@ import yaml
 with open("config.yml") as f:
     config: dict = yaml.safe_load(f)
 
-model_list: List[dict] = []
+dataset_list = []
+
+if "mmlu_test" in config["datasets"]:
+    print("Loading MMLU...")
+    mmlu = datasets.load_dataset("cais/mmlu", "all")
+    # dataset_list.append(dict(name="mmlu_val", type="multiple_choice", data=mmlu["validation"]))
+    dataset_list.append(dict(name="mmlu_test", type="multiple_choice", data=mmlu["test"]))
+
+if "gsm8k_test" in config["datasets"]:
+    print("Loading GSM8K...")
+    gsm8k = datasets.load_dataset("openai/gsm8k", "main")
+    # dataset_list.append(dict(name="gsm8k_train", type="math", data=gsm8k["train"]))
+    dataset_list.append(dict(name="gsm8k_test", type="math", data=gsm8k["test"]))
+
+jobs: List[dict] = []
 for model in config["models"]:
     name: str = model["name"]
     quantizations: List[str] = model.get("quantizations", config["quantizations"])
@@ -54,17 +68,20 @@ for model in config["models"]:
 
         file_size: int = os.path.getsize(path_model)
 
-        model_list.append(dict(
-            name=name,
-            quantization=quant,
-            path_model=path_model,
-            file_size=file_size,
-            parallel=parallel,
-            gpus_per_job=gpus_per_job,
-            prompt_types=prompt_types,
-        ))
+        for dataset in dataset_list:
+            for prompt_type in prompt_types:
+                jobs.append(dict(
+                    name=name,
+                    quantization=quant,
+                    path_model=path_model,
+                    file_size=file_size,
+                    parallel=parallel,
+                    gpus_per_job=gpus_per_job,
+                    dataset=dataset,
+                    prompt_type=prompt_type,
+                ))
 
-model_list = sorted(model_list, key=lambda m: m["file_size"], reverse=True)
+jobs = sorted(jobs, key=lambda job: len(job["dataset"]["data"]) * job["file_size"], reverse=True)
 
 CTX_SIZE = 4096
 
@@ -92,20 +109,6 @@ def get_grammar_multiple_choice(num_choices: int) -> str:
 TEMPLATE_RESPONSE_MATH = """The correct answer is """
 TEMPLATE_RESPONSE_MATH_NORMAL_2 = """My final answer is """
 TEMPLATE_GRAMMAR_MATH = "root ::= [0-9]+.*"
-
-dataset_list = []
-
-if "mmlu_test" in config["datasets"]:
-    print("Loading MMLU...")
-    mmlu = datasets.load_dataset("cais/mmlu", "all")
-    # dataset_list.append(dict(name="mmlu_val", type="multiple_choice", data=mmlu["validation"]))
-    dataset_list.append(dict(name="mmlu_test", type="multiple_choice", data=mmlu["test"]))
-
-if "gsm8k_test" in config["datasets"]:
-    print("Loading GSM8K...")
-    gsm8k = datasets.load_dataset("openai/gsm8k", "main")
-    # dataset_list.append(dict(name="gsm8k_train", type="math", data=gsm8k["train"]))
-    dataset_list.append(dict(name="gsm8k_test", type="math", data=gsm8k["test"]))
 
 
 def process_multiple_choice(example: dict) -> List[float]:
@@ -261,32 +264,36 @@ def process_math(example: dict) -> List[float]:
     return prediction
 
 
-def process_model(model, gpu_ids: List[int]):
+def process_job(job, gpu_ids: List[int]):
     port = 1337 + gpu_ids[0]
     server_address = TEMPLATE_SERVER_ADDRESS.format(port=port)
 
-    name: str = model["name"]
-    quant: str = model["quantization"]
-    path_model: str = model["path_model"]
-    parallel: int = model["parallel"]
-    prompt_types: List[str] = model["prompt_types"]
+    name: str = job["name"]
+    quant: str = job["quantization"]
+    path_model: str = job["path_model"]
+    parallel: int = job["parallel"]
+    dataset: dict = job["dataset"]
+    prompt_type: str = job["prompt_type"]
 
-    dir_out: str = os.path.join("out", "bench", name, quant)
+    dir_out: str = os.path.join("out", "bench", name, quant, dataset["name"], prompt_type)
     os.makedirs(dir_out, exist_ok=True)
 
-    all_targets_existent: bool = True
-    for ds in dataset_list:
-        ds_name = ds["name"]
-        for prompt_type in prompt_types:
-            target: str = os.path.join(dir_out, ds_name, f"pred-{prompt_type}.npy")
-            if not os.path.exists(target):
-                all_targets_existent = False
-                break
-    if all_targets_existent:
+    target: str = os.path.join(dir_out, f"pred.npy")
+    if os.path.exists(target):
         return
 
-    with open(os.path.join(dir_out, "model.yml"), "w") as f:
-        yaml.safe_dump(model, f)
+    with open(os.path.join(dir_out, "job.yml"), "w") as f:
+        yaml.safe_dump(job, f)
+
+    if dataset["type"] == "multiple_choice":
+        labels = np.array([ex["answer"] for ex in dataset["data"]], dtype=np.int64)
+    elif dataset["type"] == "math":
+        labels = np.array([int(ex["answer"].split()[-1].replace(",", "")) for ex in dataset["data"]], dtype=np.int64)
+    else:
+        assert False
+    if config["max_examples_per_dataset"] >= 0:
+        labels = labels[:config["max_examples_per_dataset"]]
+    np.save(os.path.join(dir_out, "labels.npy"), labels)
 
     env: Dict[str, str] = dict(
         CUDA_VISIBLE_DEVICES=",".join([str(g) for g in gpu_ids]),
@@ -312,51 +319,30 @@ def process_model(model, gpu_ids: List[int]):
             except requests.ConnectionError:
                 pass
 
-        for ds in dataset_list:
-            ds_name = ds["name"]
-            ds_type = ds["type"]
+        data_modded = []
+        for ex in dataset["data"]:
+            ex_copy = deepcopy(ex)
+            ex_copy["name"] = f"{name}-{quant}-{dataset['name']}-{prompt_type}"
+            ex_copy["server_address"] = server_address
+            ex_copy["prompt_type"] = prompt_type
+            data_modded.append(ex_copy)
+        if config["max_examples_per_dataset"] >= 0:
+            data_modded = data_modded[:config["max_examples_per_dataset"]]
 
-            os.makedirs(os.path.join(dir_out, ds_name), exist_ok=True)
-
-            if ds_type == "multiple_choice":
-                labels = np.array([ex["answer"] for ex in ds["data"]], dtype=np.int64)
-            elif ds_type == "math":
-                labels = np.array([int(ex["answer"].split()[-1].replace(",", "")) for ex in ds["data"]], dtype=np.int64)
+        print(f"Start: {name}-{quant}, {dataset['name']}, {prompt_type}, gpu_ids={gpu_ids}")
+        t0 = time()
+        with Pool(parallel) as pool:
+            chunksize: int = 1 if config["debug"] else min(10, max(1, len(data_modded) // (4*parallel)))
+            if dataset['type'] == "multiple_choice":
+                predictions = pool.map(process_multiple_choice, data_modded, chunksize=chunksize)
+                predictions = np.array(predictions, dtype=np.float64)
+            elif dataset['type'] == "math":
+                predictions = pool.map(process_math, data_modded, chunksize=chunksize)
+                predictions = np.array(predictions, dtype=np.int64)
             else:
                 assert False
-            if config["max_examples_per_dataset"] >= 0:
-                labels = labels[:config["max_examples_per_dataset"]]
-            np.save(os.path.join(dir_out, ds_name, "labels.npy"), labels)
-
-            for prompt_type in prompt_types:
-                target: str = os.path.join(dir_out, ds_name, f"pred-{prompt_type}.npy")
-                if os.path.exists(target):
-                    continue
-
-                data_modded = []
-                for ex in ds["data"]:
-                    ex_copy = deepcopy(ex)
-                    ex_copy["name"] = f"{name}-{quant}-{ds_name}-{prompt_type}"
-                    ex_copy["server_address"] = server_address
-                    ex_copy["prompt_type"] = prompt_type
-                    data_modded.append(ex_copy)
-                if config["max_examples_per_dataset"] >= 0:
-                    data_modded = data_modded[:config["max_examples_per_dataset"]]
-
-                print(f"Start: {name}-{quant}, {ds_name}, {prompt_type}, gpu_ids={gpu_ids}")
-                t0 = time()
-                with Pool(parallel) as pool:
-                    chunksize: int = 1 if config["debug"] else min(10, max(1, len(data_modded) // (4*parallel)))
-                    if ds_type == "multiple_choice":
-                        predictions = pool.map(process_multiple_choice, data_modded, chunksize=chunksize)
-                        predictions = np.array(predictions, dtype=np.float64)
-                    elif ds_type == "math":
-                        predictions = pool.map(process_math, data_modded, chunksize=chunksize)
-                        predictions = np.array(predictions, dtype=np.int64)
-                    else:
-                        assert False
-                print(f"Done: {name}-{quant}, {ds_name}, {prompt_type}, time={time() - t0:.2f}s")
-                np.save(target, predictions)
+        print(f"Done: {name}-{quant}, {dataset['name']}, {prompt_type}, time={time() - t0:.2f}s")
+        np.save(target, predictions)
 
         server_process.terminate()
 
@@ -364,51 +350,51 @@ def process_model(model, gpu_ids: List[int]):
 lock = threading.Lock()
 
 
-def find_model(num_gpus: int):
+def find_job(num_gpus: int):
     ret = None
     lock.acquire()
-    for i in range(len(model_list)):
-        if model_list[i]["gpus_per_job"] == num_gpus:
-            ret = model_list.pop(i)
+    for i in range(len(jobs)):
+        if jobs[i]["gpus_per_job"] == num_gpus:
+            ret = jobs.pop(i)
             break
     lock.release()
     return ret
 
 
-def process_models(gpu_ids: List[int]):
-    model = find_model(len(gpu_ids))
-    while model is not None:
-        process_model(model, gpu_ids)
-        model = find_model(len(gpu_ids))
+def process_jobs(gpu_ids: List[int]):
+    job = find_job(len(gpu_ids))
+    while job is not None:
+        process_job(job, gpu_ids)
+        job = find_job(len(gpu_ids))
 
 
 gpu_ids: List[int] = list(range(6))
 threads_2 = []
 
-process_models(gpu_ids)
-process_models(gpu_ids[:5])
+process_jobs(gpu_ids)
+process_jobs(gpu_ids[:5])
 
-threads_2.append(threading.Thread(target=process_models, args=[gpu_ids[4:]]))
+threads_2.append(threading.Thread(target=process_jobs, args=[gpu_ids[4:]]))
 threads_2[-1].start()
-process_models(gpu_ids[:4])
+process_jobs(gpu_ids[:4])
 
-threads_2.append(threading.Thread(target=process_models, args=[gpu_ids[2:4]]))
+threads_2.append(threading.Thread(target=process_jobs, args=[gpu_ids[2:4]]))
 threads_2[-1].start()
-process_models(gpu_ids[:2])
+process_jobs(gpu_ids[:2])
 
 for thread in threads_2:
     thread.join()
 
-thread_3 = threading.Thread(target=process_models, args=[gpu_ids[3:]])
+thread_3 = threading.Thread(target=process_jobs, args=[gpu_ids[3:]])
 thread_3.start()
-process_models(gpu_ids[:3])
+process_jobs(gpu_ids[:3])
 thread_3.join()
 
 threads_1 = []
 for i in range(1, 6):
-    threads_1.append(threading.Thread(target=process_models, args=[gpu_ids[i:i+1]]))
+    threads_1.append(threading.Thread(target=process_jobs, args=[gpu_ids[i:i+1]]))
     threads_1[-1].start()
-process_models(gpu_ids[:1])
+process_jobs(gpu_ids[:1])
 
 for thread in threads_1:
     thread.join()
