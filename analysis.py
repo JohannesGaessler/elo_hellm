fb58e3dd5a0bdd5e1c25cc1b8617ea10759339ff
Johannes Gäßler <johannesg@5d6.de>
2025-06-15 17:42:41 +0200

    Refactor analysis code, use kafe2

--- analysis.py
+++ analysis.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
 import os
+from typing import Iterable
 
 from adjustText import adjust_text
 from iminuit import Minuit
+from inspect import Parameter, Signature
+from kafe2 import CustomFit
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import chi2
@@ -23,10 +26,24 @@ os.makedirs(DIR_OUT, exist_ok=True)
 
 results: dict[tuple[str, str], dict] = dict()
 for model in config.models:
-    for dataset in model.datasets:
+    for model_scores in model.datasets:
         for prompt_type in model.prompt_types:
-            benchmark = get_benchmark(dataset, prompt_type)
+            benchmark = get_benchmark(model_scores, prompt_type)
             results[(model.name, benchmark.database_name())] = np.array(benchmark.get_results(model.name), dtype=np.int64)
+
+
+class BenchmarkModelScores:
+    name: str
+    ncorrect: np.ndarray
+    ntest: int
+    floor: float
+
+    def __init__(self, name: str, ncorrect: Iterable[int], ntest: int, floor: float):
+        self.name = name
+        self.ncorrect = np.asarray(ncorrect)
+        self.ntest = ntest
+        self.floor = floor
+
 
 ncorrect_total = 0
 ntest_total = 0
@@ -89,7 +106,7 @@ for dataset in config.datasets:
         print(tabulate(rows, headers=["Model", "File size [GiB]", "Correct answers", "", "Pareto frontier?"], tablefmt="github"))
         print()
 
-        model_scores.append(dict(name=name, ncorrect=ncorrect, ntest=ntest, floor=benchmark.score_rng))
+        model_scores.append(BenchmarkModelScores(name, ncorrect, ntest, benchmark.score_rng))
 print(f"Total correct answers: {ncorrect_total}/{ntest_total}")
 print()
 
@@ -126,11 +143,13 @@ def get_nll(pars: np.ndarray, wr_err: float) -> float:
 
     nll = 0.0
     for i, ms_i in enumerate(model_scores):
-        wr = get_winrate(elos_models, elos_datasets[i], scales[i], ms_i["floor"])
-        err = np.sqrt(wr * (1.0 - wr) / ms_i["ntest"] + wr_err ** 2)
-        residuals = wr - ms_i["ncorrect"] / ms_i["ntest"]
+        wr = get_winrate(elos_models, elos_datasets[i], scales[i], ms_i.floor)
+        err = np.sqrt(wr * (1.0 - wr) / ms_i.ntest + wr_err ** 2)
+        residuals = wr - ms_i.ncorrect / ms_i.ntest
         nll += np.sum(np.square(residuals / err))
         # nll -= 2.0 * np.sum(binom.logpmf(k=ms_i["ncorrect"], n=ms_i["ntest"], p=wr))
+    if scales[-1] < 0:
+        nll += 1e6 * scales[-1] ** 2
     return nll
 
 
@@ -141,73 +160,82 @@ print(f"Pre-fit cost: {get_nll(starting_pars, 0.0):.2f}")
 
 ndf = -len(starting_pars)
 for ms in model_scores:
-    ndf += ms["ncorrect"].shape[0]
+    ndf += ms.ncorrect.shape[0]
 
 
-def get_minuit(wr_err: float):
-    def func(pars):
+def get_par_name(s: str):
+    return s.replace("-", "_").replace(".", "")
+
+
+def get_fit(wr_err: float) -> CustomFit:
+    def func(*pars):
         return get_nll(pars, wr_err)
-    m = Minuit(func, starting_pars)
-    m.errordef = 1.0
-    for i in range(len(starting_scales)):
-        m.limits[i] = (10, 1000)
-        # m.fixed[i] = True
-    m.migrad()
-    m.hesse()
-    return m
+    pars: list[Parameter] = []
+    for ms in model_scores[:-1]:
+        pars.append(Parameter(name=f"scale_{get_par_name(ms.name)}", kind=Parameter.POSITIONAL_OR_KEYWORD, default=400))
+    for ms in model_scores:
+        pars.append(Parameter(name=f"elo_{get_par_name(ms.name)}", kind=Parameter.POSITIONAL_OR_KEYWORD, default=1500))
+    for model in config.models[:-1]:
+        pars.append(Parameter(name=f"elo_{get_par_name(model.name)}", kind=Parameter.POSITIONAL_OR_KEYWORD, default=1500))
+    func.__signature__ = Signature(pars)
+
+    fit = CustomFit(func)
+    for ms in model_scores[:-1]:
+        fit.limit_parameter(f"scale_{ms.name}", lower=1e-6)
+    fit.do_fit()
+    return fit
 
 
 tol = 1e-4
 wr_err_low = 0.00
 wr_err_high = 0.01
-m_low = get_minuit(wr_err_low)
-if m_low.fval <= ndf + tol:
+fit_low = get_fit(wr_err_low)
+if fit_low.cost_function_value <= ndf + tol:
     wr_err_final = wr_err_low
-    m_final = m_low
+    fit_final = fit_low
 else:
-    m_high = get_minuit(wr_err_high)
+    fit_high = get_fit(wr_err_high)
     i = 0
-    while m_high.fval > ndf:
+    while fit_high.cost_function_value > ndf:
         wr_err_low = wr_err_high
         wr_err_high += 0.01
-        m_low = m_high
-        m_high = get_minuit(wr_err_high)
+        fit_low = fit_high
+        fit_high = get_fit(wr_err_high)
         i += 1
         assert i <= 10
     wr_err_test = (wr_err_low + wr_err_high) / 2
-    m_test = get_minuit(wr_err_test)
+    fit_test = get_fit(wr_err_test)
 
     i = 0
-    while abs(m_test.fval - ndf) > tol and i < 10:
-        if m_test.fval > ndf:
+    while abs(fit_test.cost_function_value - ndf) > tol and i < 10:
+        if fit_test.cost_function_value > ndf:
             wr_err_low = wr_err_test
-            m_low = m_test
+            fit_low = fit_test
         else:
             wr_err_high = wr_err_test
-            m_high = m_test
+            fit_high = fit_test
         wr_err_test = (wr_err_low + wr_err_high) / 2
-        m_test = get_minuit(wr_err_test)
+        fit_test = get_fit(wr_err_test)
         i += 1
-    wr_err_final = wr_err_low
-    m_final = m_test
+    wr_err_final = wr_err_test
+    fit_final = fit_test
 
+fit_final.report(asymmetric_parameter_errors=True)
 print(f"wr_err_final={100*wr_err_final:.2f}%")
 
-nll_sat = m_final.fval
+nll_sat = fit_final.cost_function_value
 # for ms in model_scores:
-    # nll_sat += 2.0 * np.sum(binom.logpmf(k=ms["ncorrect"], n=ms["ntest"], p=ms["ncorrect"]/ms["ntest"]))
+    # nll_sat += 2.0 * np.sum(binom.logpmf(k=ms.ncorrect, n=ms.ntest, p=ms.ncorrect/ms.ntest))
 print(f"chi2 / NDF: {nll_sat:.2f}/{ndf} = {nll_sat/ndf if ndf > 0 else np.nan:.2f}")
 chi2_prob = 1.0 - chi2.cdf(nll_sat, ndf)
 print(f"chi2 probability: {100*chi2_prob:.2f}%")
 print()
 
-print(f"Post-fit cost: {m_final.fval:.2f}")
-print()
+final_scales, final_elos_datasets, final_elos_models = decompile_pars(fit_final.parameter_values)
+final_scales_unc, final_elos_datasets_unc, final_elos_models_unc = decompile_pars(
+    fit_final.parameter_errors, fit_final.parameter_cov_mat)
 
-final_scales, final_elos_datasets, final_elos_models = decompile_pars(m_final.values)
-final_scales_unc, final_elos_datasets_unc, final_elos_models_unc = decompile_pars(m_final.errors, m_final.covariance)
-
-dataset_elo_scale_unc = sorted(
+ms_elo_scale_unc = sorted(
     zip(model_scores, final_elos_datasets, final_scales, final_elos_datasets_unc, final_scales_unc),
     key=lambda mesu: mesu[1], reverse=True
 )
@@ -217,8 +245,8 @@ model_elo_unc = sorted(
 )
 
 rows = []
-for dataset, elo, scale, elo_unc, scale_unc in dataset_elo_scale_unc:
-    rows.append([dataset["name"], f"{elo:.2f}±{elo_unc:.2f}", f"{scale:.2f}±{scale_unc:.2f}"])
+for ms, elo, scale, elo_unc, scale_unc in ms_elo_scale_unc:
+    rows.append([ms.name, f"{elo:.2f}±{elo_unc:.2f}", f"{scale:.2f}±{scale_unc:.2f}"])
 print("## Final Dataset Elo Scores")
 print()
 print(tabulate(rows, headers=["Dataset", "Elo score", "Scale"], tablefmt="github"))
@@ -240,9 +268,9 @@ print()
 num_within_sigma = [0, 0, 0]
 num_total = 0
 for i, ms_i in enumerate(model_scores):
-    wr_data = ms_i["ncorrect"] / ms_i["ntest"]
+    wr_data = ms_i.ncorrect / ms_i.ntest
     wr_data_unc = np.sqrt(wr_data * (1.0 - wr_data) / wr_data.shape[0])
-    wr_elo = get_winrate(final_elos_models, final_elos_datasets[i], final_scales[i], ms_i["floor"])
+    wr_elo = get_winrate(final_elos_models, final_elos_datasets[i], final_scales[i], ms_i.floor)
     abs_diffs = np.abs(wr_data - wr_elo)
     for j, abs_diff_j in enumerate(abs_diffs):
         if abs_diff_j <= 1*wr_data_unc[j]:
@@ -253,7 +281,7 @@ for i, ms_i in enumerate(model_scores):
             num_within_sigma[2] += 1
         num_total += 1
         if abs_diff_j > wr_data_unc[j]:
-            print(f"dataset={ms_i['name']} model={config.models[j].name}: wr_elo={100*wr_elo[j]:.4f}% wr_data={100*wr_data[j]:.4f}%")
+            print(f"dataset={ms_i.name} model={config.models[j].name}: wr_elo={100*wr_elo[j]:.4f}% wr_data={100*wr_data[j]:.4f}%")
 print(f"Within 1 sigma: {100*num_within_sigma[0]/num_total:.2f}%")
 print(f"Within 2 sigma: {100*num_within_sigma[1]/num_total:.2f}%")
 print(f"Within 3 sigma: {100*num_within_sigma[2]/num_total:.2f}%")
@@ -262,15 +290,15 @@ for fed, fs, ms in zip(final_elos_datasets, final_scales, model_scores):
     plt.figure()
 
     x_plot = np.linspace(1000, 2000, 201)
-    y_plot = get_winrate(x_plot, fed, fs, ms["floor"])
+    y_plot = get_winrate(x_plot, fed, fs, ms.floor)
     plt.plot(x_plot, y_plot)
     plt.fill_between(x_plot, y_plot - wr_err_final, y_plot + wr_err_final, alpha=0.2)
 
-    wr_data = ms["ncorrect"] / ms["ntest"]
-    wr_data_unc = np.sqrt(wr_data * (1.0 - wr_data) / ms["ntest"])
+    wr_data = ms.ncorrect / ms.ntest
+    wr_data_unc = np.sqrt(wr_data * (1.0 - wr_data) / ms.ntest)
     plt.errorbar(final_elos_models, wr_data, wr_data_unc, marker=".", linestyle="none")
-    if ms["floor"] != 0.0:
-        plt.hlines(ms["floor"], x_plot[0], x_plot[-1], colors="black", linestyles=":")
+    if ms.floor != 0.0:
+        plt.hlines(ms.floor, x_plot[0], x_plot[-1], colors="black", linestyles=":")
     texts = [plt.text(fem, wrd, m.name) for fem, wrd, m in zip(final_elos_models, wr_data, config.models)]
     plt.xlim(x_plot[0], x_plot[-1])
     plt.ylim(0, 1)
@@ -278,10 +306,10 @@ for fed, fs, ms in zip(final_elos_datasets, final_scales, model_scores):
         arrowprops=dict(arrowstyle="->", color="lightgray"), expand=(1.35, 2.3),
         force_explode=(0.4, 1.0), ensure_inside_axes=True, max_move=100)
 
-    plt.title(ms["name"])
+    plt.title(ms.name)
     plt.xlabel("Elo")
     plt.ylabel("Model winrate vs. benchmark")
-    plt.savefig(os.path.join(DIR_OUT, f"{ms['name']}-elo-winrate.png"), dpi=240)
+    plt.savefig(os.path.join(DIR_OUT, f"{ms.name}-elo-winrate.png"), dpi=240)
 
 plot_data = []
 for i, model_i in enumerate(config.models):
