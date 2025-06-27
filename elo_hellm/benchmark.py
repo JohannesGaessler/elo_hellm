8f9d859d4e7ad4d5d2599c5d962ffcbe3eed438d
Johannes Gäßler <johannesg@5d6.de>
2025-06-27 13:35:06 +0200

    WIP

--- elo_hellm/benchmark.py
+++ elo_hellm/benchmark.py
@@ -404,8 +404,6 @@ class BenchmarkChess960(Benchmark):
             assert len(query) == 1
             moves: list[dict] = json.loads(query[0][0])
         else:
-            assert local_data.stockfish.is_fen_valid(state)
-            local_data.stockfish.set_fen_position(state)
             moves: list[dict] = local_data.stockfish.get_top_moves(BenchmarkChess960.nchoices)
             moves = sorted(moves, key=BenchmarkChess960.move_to_key, reverse=True)
             BenchmarkChess960.add_random_moves(moves, iex, turn)
