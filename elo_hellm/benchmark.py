77fac9f110e1f2092ab1f2b1d2bd7691a0114080
Johannes Gäßler <johannesg@5d6.de>
2025-06-28 09:34:09 +0200

    WIP

--- elo_hellm/benchmark.py
+++ elo_hellm/benchmark.py
@@ -424,7 +424,7 @@ class BenchmarkChess960(Benchmark):
         else:
             moves: list[dict] = local_data.stockfish.get_top_moves(BenchmarkChess960.nchoices)
             moves = sorted(moves, key=BenchmarkChess960.move_to_key, reverse=True)
-            assert len(moves) >= 1, f"state={state}"
+            assert len(moves) >= 1, f"state={state} visual:\n{local_data.stockfish.get_board_visual()}"
             worst_legal_move: int = len(moves) - 1
             BenchmarkChess960.add_random_moves(moves, iex, i_gen, turn)
 
